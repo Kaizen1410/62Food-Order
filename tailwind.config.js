70422f06c484da1b804185b78712e7c9a74aad21
Ahmad Muhajir <amuhajir.syamlan@gmail.com>
2023-10-02 13:30:56 +0700

    assets & theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'p1': ['18px', {
+          lineHeight: 'normal',
+          fontWeight: '500'
+        }],
+        'p2': ['13px', {
+          lineHeight: 'normal',
+          fontWeight: '400'
+        }],
+      },
+      colors: {
+        'primary': '#F48E28',
+        'secondary': '#F5DDC4',
+        'gray': '#B6B6B6'
+      }
+    },
   },
   plugins: [],
 }
